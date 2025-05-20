900256229509db02b6402e388494705bd03f5c4f
ierluna <lunjavag@gmail.com>
2025-05-20 16:51:58 -0300

    se agrega comportamiento basico de busqueda

--- src/main/java/models/Producto.java
+++ src/main/java/models/Producto.java
@@ -23,11 +23,16 @@ public class Producto {
         return id;
     }
 
-    public void reportarProducto() {
-        System.out.printf("Nombre del producto: %s  Precio: %f  Stock: %d  ID: %d", nombre, precio, stock, id  );
+    public String getNombre() {
+        return nombre;
     }
 
-    public static void reportarListaDeProductos(ArrayList<Producto> listaProducto){
+    public void reportarProducto() {
+        System.out.printf("Nombre del producto: %s  Precio: %f  Stock: %d  ID: %d", nombre, precio, stock, id);
+        System.out.println("\n");
+    }
+
+    public static void reportarListaDeProductos(ArrayList<Producto> listaProducto) {
         System.out.println("Lista de productos: ");
 
         for (Producto producto : listaProducto) {
@@ -36,4 +41,22 @@ public class Producto {
             System.out.println("\n");
         }
     }
+
+    public static void buscarPorNombre(ArrayList<Producto> listaProductos, String nombreBuscado) {
+        boolean encontrado = false;
+
+        for (Producto producto : listaProductos) {
+            if (producto.getNombre().equalsIgnoreCase(nombreBuscado)) {
+                producto.reportarProducto();// se encontró el producto
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("No se encontró el producto");
+            System.out.println("\n");
+        }
+    }
 }
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,19 @@ public class Main {
     public static void main(String[] args) {
         Producto pepsi = new Producto("Pepsi",500,10);
         Producto cocaCola = new Producto("cocaCola",1000,20);
+        Producto cunnington = new Producto("cunnington",800,15);
 
         ArrayList<Producto> listaDeBebidas = new ArrayList<>();
 
         listaDeBebidas.add(pepsi);
         listaDeBebidas.add(cocaCola);
+        listaDeBebidas.add(cunnington);
 
-        Producto.reportarListaDeProductos(listaDeBebidas);
+        //Producto.reportarListaDeProductos(listaDeBebidas);
+
+        Producto.buscarPorNombre(listaDeBebidas, "cocaCola");
+        Producto.buscarPorNombre(listaDeBebidas, "cunnington");
+
+
     }
 }
