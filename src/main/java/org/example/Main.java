d3866cfb0fa262c7c81739eec28dce4ea85bbc5d
ierluna <lunjavag@gmail.com>
2025-05-24 14:40:22 -0300

    se agrega menu basico

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,59 @@ package org.example;
 import models.Producto;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Producto pepsi = new Producto("Pepsi",500,10);
+        Scanner entrada = new Scanner(System.in);
+        {
+
+            int opcionUsuario;
+            do {
+                System.out.println("""
+                        1) Agregar producto
+                        2) Listar productos
+                        3) Buscar/Actualizar producto
+                        4) Eliminar producto
+                        5) Crear un pedido
+                        6) Listar pedidos
+                        7) Salir
+                        
+                        Elija una opción:
+                        """);
+                opcionUsuario = entrada.nextInt();
+
+                switch (opcionUsuario) {
+                    case 1:
+                        System.out.println("Agregar producto");
+                        break;
+                    case 2:
+                        System.out.println("Listar producto");
+                        break;
+                    case 3:
+                        System.out.println("Buscar/Actualizar producto");
+                        break;
+                    case 4:
+                        System.out.println("Eliminar producto...");
+                        break;
+                    case 5:
+                        System.out.println("Crear un pedido...");
+                        break;
+                    case 6:
+                        System.out.println("Listar pedidos...");
+                        break;
+                    case 7:
+                        System.out.println("¡Gracias por usar la app!");
+                        break;
+                    default:
+                        System.out.println("Opción incorrecta");
+                }
+            } while (opcionUsuario != 7);
+        }
+
+/*      Producto pepsi = new Producto("Pepsi",500,10);
         Producto cocaCola = new Producto("cocaCola",1000,20);
 
         ArrayList<Producto> listaDeBebidas = new ArrayList<>();
@@ -16,6 +63,6 @@ public class Main {
         listaDeBebidas.add(pepsi);
         listaDeBebidas.add(cocaCola);
 
-        Producto.reportarListaDeProductos(listaDeBebidas);
+        Producto.reportarListaDeProductos(listaDeBebidas);*/
     }
 }
